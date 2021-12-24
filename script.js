1c6f591b1b3558c81b1711ac1a2d8cadab33c84c
Carlos Caicedo <calicheccmusica@gmail.com>
2021-12-23 22:34:08 -0800

    second commit

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log("The Matrix is always loaded ...")
+
+function playVideo(vid) {
+    console.log(vid);
+    vid.play();
+}
+
+function pauseVideo(vid) {
+    console.log(vid);
+    vid.pause();
+    // vid.currentTime = 0;
+}
